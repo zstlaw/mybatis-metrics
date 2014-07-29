bbf2230d2fad0a5433070ac1fc58bf7b631ae294
Tomasz Guzik <tomek@tguzik.com>
2014-07-30 00:01:26 +0200

    Added stub for the loader unit test

---
+++ src/test/java/com/tguzik/metrics/mybatis/InstrumentationMetricSetLoaderTest.java
@@ -0,0 +1,18 @@
+package com.tguzik.metrics.mybatis;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InstrumentationMetricSetLoaderTest {
+
+    @Test
+    public void testLoad() throws Exception {
+
+    }
+
+    @Test
+    public void testDeriveBaseName() throws Exception {
+
+    }
+}
